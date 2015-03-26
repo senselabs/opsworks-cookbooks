396c8ec5cd51f3d94ada3e41424b6e469657729a
Gabriel Kirkpatrick <g@be-k.biz>
2015-03-25 23:36:48 -0400

    Maybe this one will work

--- deploy/definitions/opsworks_rails.rb
+++ deploy/definitions/opsworks_rails.rb
@@ -18,6 +18,20 @@ define :opsworks_rails do
   #   end
   # end
 
+  # write out the environment variables
+  template "#{deploy[:deploy_to]}/shared/config/application.yml" do
+    cookbook "rails"
+    source "application.yml.erb"
+    mode "0660"
+    owner deploy[:user]
+    group deploy[:group]
+    variables(env: OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables]))
+
+    only_if do
+      deploy.has_key?(:puma)
+    end
+  end
+
   execute "symlinking subdir mount if necessary" do
     command "rm -f /var/www/#{deploy[:mounted_at]}; ln -s #{deploy[:deploy_to]}/current/public /var/www/#{deploy[:mounted_at]}"
     action :run
