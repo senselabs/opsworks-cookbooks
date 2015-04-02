efaea9254514fec79c38c4191753e8525cd5b11f
Gabriel Kirkpatrick <g@be-k.biz>
2015-04-02 15:18:10 -0400

    Moved database.yml generation from deploy to configure for puma apps

--- deploy/definitions/opsworks_rails.rb
+++ deploy/definitions/opsworks_rails.rb
@@ -32,6 +32,22 @@ define :opsworks_rails do
     end
   end
 
+  template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+    cookbook "rails"
+    source "database.yml.erb"
+    mode "0660"
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :database => deploy[:database],
+      :environment => deploy[:rails_env]
+      )
+
+    only_if do
+      deploy[:database][:host].present? and deploy.has_key?(:puma)
+    end
+  end
+
   execute "symlinking subdir mount if necessary" do
     command "rm -f /var/www/#{deploy[:mounted_at]}; ln -s #{deploy[:deploy_to]}/current/public /var/www/#{deploy[:mounted_at]}"
     action :run
