403f2316ed754828448aaf46713df11ba9b316a9
Gabriel Kirkpatrick <g@be-k.biz>
2015-04-21 21:08:40 -0400

    Commented out the wrong config file

--- deploy/definitions/opsworks_rails.rb
+++ deploy/definitions/opsworks_rails.rb
@@ -19,34 +19,34 @@ define :opsworks_rails do
   # end
 
   # write out the environment variables
-  # template "#{deploy[:deploy_to]}/shared/config/application.yml" do
-  #   cookbook "rails"
-  #   source "application.yml.erb"
-  #   mode "0660"
-  #   owner deploy[:user]
-  #   group deploy[:group]
-  #   variables(env: OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables]))
-
-  #   only_if do
-  #     deploy.has_key?(:puma)
-  #   end
-  # end
-
-  template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+  template "#{deploy[:deploy_to]}/shared/config/application.yml" do
     cookbook "rails"
-    source "database.yml.erb"
+    source "application.yml.erb"
     mode "0660"
     owner deploy[:user]
     group deploy[:group]
-    variables(
-      :database => deploy[:database],
-      :environment => deploy[:rails_env]
-      )
+    variables(env: OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables]))
 
     only_if do
       deploy.has_key?(:puma)
     end
   end
+
+  # template "#{deploy[:deploy_to]}/shared/config/database.yml" do
+  #   cookbook "rails"
+  #   source "database.yml.erb"
+  #   mode "0660"
+  #   owner deploy[:user]
+  #   group deploy[:group]
+  #   variables(
+  #     :database => deploy[:database],
+  #     :environment => deploy[:rails_env]
+  #     )
+
+  #   only_if do
+  #     deploy.has_key?(:puma)
+  #   end
+  # end
 
   execute "symlinking subdir mount if necessary" do
     command "rm -f /var/www/#{deploy[:mounted_at]}; ln -s #{deploy[:deploy_to]}/current/public /var/www/#{deploy[:mounted_at]}"
